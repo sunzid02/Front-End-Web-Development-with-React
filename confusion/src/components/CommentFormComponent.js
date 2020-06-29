ab34989906f24846e69c49b289c792243bef7574
sunzid02 <sunzid02@gmail.com>
2020-06-29 19:35:58 +0600

    assignment 3 before

--- confusion/src/components/CommentFormComponent.js
+++ confusion/src/components/CommentFormComponent.js
@@ -1,22 +1,17 @@
-import React, { Component } from "react";
-import { Card, CardImg, CardImgOverlay, CardTitle, Breadcrumb, BreadcrumbItem, CardBody, CardText } from "reactstrap";
-import { Link } from 'react-router-dom';
-import {
+import React, { Component } from 'react';
+
+import {   
     Button, Modal, ModalBody, ModalHeader, Label, Row, Col
 } from "reactstrap";
 
 import { Control, LocalForm, Errors } from 'react-redux-form';
 
-
-
-
-/**........................ comment component ends ................................................. */
 //// validators
 const required = (val) => val && val.length; //value > 0
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
 const minLength = (len) => (val) => (val) && (val.length >= len);
 
-class CommentForm extends Component {
+class CommentForm extends Component{
 
     constructor(props) {
         super(props);
@@ -45,8 +40,8 @@ class CommentForm extends Component {
     }
 
 
-    render() {
-        return (
+    render(){
+        return(
             <React.Fragment>
                 <Button outline onClick={this.toggleCommentFormModal}>
                     <span className="fa fa-comments fa-lg"></span> Submit Comment
@@ -57,9 +52,9 @@ class CommentForm extends Component {
                 <Modal isOpen={this.state.isCommentFormModalOpen} toggle={this.toggleCommentFormModal} >
                     <ModalHeader toggle={this.toggleCommentFormModal}> Submit Comment </ModalHeader>
                     <ModalBody>
-
+                       
                         <LocalForm onSubmit={(values) => this.handleCommentFormSubmit(values)}>
-
+                          
                             {/* rating */}
                             <Row className="form-group">
                                 <Label htmlFor="rating" md={12} >Rating</Label>
@@ -86,7 +81,7 @@ class CommentForm extends Component {
                                         messages={{
                                             required: 'Required',
                                         }}
-                                    />
+                                    />  
                                 </Col>
                             </Row>
 
@@ -111,7 +106,7 @@ class CommentForm extends Component {
                                             minLength: 'Must be greater than 2 characters',
                                             maxLength: 'Must be 15 characters or less'
                                         }}
-                                    />
+                                    />                                    
                                 </Col>
                             </Row>
 
@@ -136,7 +131,7 @@ class CommentForm extends Component {
                                         messages={{
                                             required: 'Required',
                                         }}
-                                    />
+                                    />  
                                 </Col>
 
                             </Row>
@@ -161,106 +156,4 @@ class CommentForm extends Component {
     }
 }
 
-/**........................ comment component ends ................................................. */
-
-
-
-
-
-
-
-
-    function RenderDish({dish}) {
-        if (dish != null) {
-            return (
-                <div className='col-12 col-md-5 m-1'>
-                    <Card>
-                        <CardImg width="100%" src={dish.image} alt={dish.name} />
-                        <CardBody>
-                            <CardTitle> {dish.name}</CardTitle>
-                            <CardText> {dish.description} </CardText>
-                        </CardBody>
-                    </Card>
-                </div>   
-            );
-        }
-        else {
-            return (
-                <div></div>
-            );
-        }
-    }
-
-    function RenderComments({dish,comments}){
-        if (comments == null) {
-            return (<div></div>)
-        }
-        const cmnts = comments.map(comment => {
-            return (
-                
-                <li key={comment.id}>
-                    <p>{comment.comment}</p>
-                    <p>-- {comment.author},
-                    &nbsp;
-                    {new Intl.DateTimeFormat('en-US', {
-                        year: 'numeric',
-                        month: 'long',
-                        day: '2-digit'
-                    }).format(new Date(comment.date))}
-                    </p>
-                </li>
-
-            )
-        })
-        return (
-            <div className='col-12 col-md-5 m-1'>
-                <h4> Comments </h4>
-                <ul className='list-unstyled'>
-                    {cmnts}
-                </ul>
-                <CommentForm dish={dish} comments={comments} />
-            </div>
-        )
-    }
-
-
-    const DishDetail = (props) => {
-
-        const dish = props.dish
-        
-    
-        if (dish == null) {
-            return (<div></div>);
-        }
-
-        return (
-            <div className="container">
-                <div className="row">
-                    <Breadcrumb>
-                        <BreadcrumbItem>
-                            <Link to="/menu">Menu</Link>
-                        </BreadcrumbItem>
-                        <BreadcrumbItem active>
-                            { props.dish.name }
-                        </BreadcrumbItem>
-                    </Breadcrumb>
-
-                    <div className="col-12">
-                        <h3> {props.dish.menu}</h3>
-                        <hr />
-                    </div>
-                </div>
-
-                <div className='row'>
-                    <RenderDish dish={ props.dish } />
-                    <RenderComments dish={props.dish} comments={ props.comments } />
-                </div>
-
-
-            </div>
-        )
-    }
-
-
-
-export default DishDetail;
+export default CommentForm;
